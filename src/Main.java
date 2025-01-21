6949c8b944fe611e65d2b8391bbe34985c5783c4
Lukas Bauza <lukasbauza.lb@proton.me>
2025-01-21 13:14:48 +0000

    Default gateway added to PC.java and updated the menu for router functionality.
    Signed-off-by: Lukas Bauza <lukasbauza.lb@proton.me>

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList<PC> pcList = new ArrayList<>();
+        ArrayList<Router> routerList = new ArrayList<>();
 
         System.out.print("########################################\n");
         System.out.print("Welcome to the PING protocol simulation.\n");
@@ -15,38 +16,91 @@ public class Main {
         int exit = 0;
         int option = -1;
         int pcSelection = -1;
+        int routerSelection = -1;
         Scanner scanner = new Scanner(System.in);
 
         do {
             if (menu.equals("main")) {
-                printMainMenu(pcList);
+                displayMainMenu(pcList, routerList);
                 option = scanner.nextInt();
 
                 switch (option) {
-                    // Create device
                     case 1:
+                        // Create PC and add it to the list of PC's
                         pcList.add(createPC());
                         break;
 
                     case 2:
-                        // Delete device
+                        // Delete PC
                         // This will need to show a list of PC's where the user can select what PC to delete.
                         System.out.println("Select a PC to delete");
-                        // -1 becuase it is based on the index of the ArrayList of the PC.
+                        // -1 because it is based on the index of the ArrayList of the PC.
                         pcSelection = scanner.nextInt() - 1;
-                        pcList.remove(pcSelection);
+
+                        try {
+                            pcList.remove(pcSelection);
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("PC does not exist");
+                        }
+
                         break;
 
                     case 3:
-                        // Select device
+                        // Select PC
                         System.out.println("Select a PC from the list");
                         pcSelection = scanner.nextInt() - 1;
-                        menu = "pc";
+
+                        try {
+                            // Check if the PC exists within the pcList.
+                            pcList.get(pcSelection);
+                            menu = "pc";
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("PC does not exist");
+                            menu = "main";
+                        }
+
                         // Reset the option for the PC menu.
                         option = -1;
                         break;
 
                     case 4:
+                        // Create a Router
+                        routerList.add(createRouter());
+                        break;
+
+                    case 5:
+                        // Delete a Router
+                        System.out.println("Select a Router to delete");
+                        routerSelection = scanner.nextInt() - 1;
+
+                        try {
+                            routerList.remove(routerSelection);
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("Router does not exist");
+                        }
+
+                        break;
+
+                    case 6:
+                        // Select a Router
+                        System.out.println("Select a Router from the list");
+                        routerSelection = scanner.nextInt() - 1;
+
+                        try {
+                            // Check if the PC exists within the pcList.
+                            routerList.get(routerSelection);
+                            menu = "router";
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("Router does not exist");
+                            menu = "main";
+                        }
+
+                        // Reset the option for the PC menu.
+                        option = -1;
+                        break;
+
+                    case 7:
+                        // Exit the program.
                         System.out.println("Bye");
                         exit = 1;
                         break;
@@ -57,7 +111,7 @@ public class Main {
                 }
 
             } else if (menu.equals("pc")) {
-                printPCMenu(pcList);
+                displayPCMenu(pcList);
                 option = scanner.nextInt();
                 // Removes any new line characters that nextInt() didn't use. If not then any nextLine() function used
                 // on the scanner, will automatically have \n as its input.
@@ -77,7 +131,7 @@ public class Main {
                         System.out.println("Enter the new IP for the PC");
                         String ipAddressString = scanner.nextLine();
                         IPAddress ipAddress = createIP(ipAddressString);
-                        pc.setInterfaceFA00IPAddress(ipAddress);
+                        pc.setPortFA00(ipAddress);
                         break;
 
                     // Set connection with other PC
@@ -98,6 +152,17 @@ public class Main {
                     default:
                         System.out.println("Invalid option");
                         break;
+                }
+            } else if (menu.equals("router")) {
+                displayRouterMenu(routerList);
+                option = scanner.nextInt();
+                // Removes any new line characters that nextInt() didn't use. If not then any nextLine() function used
+                // on the scanner, will automatically have \n as its input.
+                scanner.nextLine();
+                Router router = routerList.get(routerSelection);
+
+                switch (option) {
+
                 }
             }
         } while (exit == 0);
@@ -132,20 +197,81 @@ public class Main {
         System.out.print("Please enter the IP address for " + name + ": ");
         String ipInput = scanner.nextLine();
         IPAddress ip = createIP(ipInput);
-        System.out.println("This is the IP address for " + name + ": " + ip.toString());
 
-        PC pc = new PC(name, ip);
-
-        System.out.println("Here is all the details of " + pc.getName() + ": ");
-        System.out.println(pc.getInterfaceFA00().getIpAddress().toString());
-        System.out.println(pc.getInterfaceFA00().getMacAddress().toString());
-
-        return pc;
+        return new PC(name, ip);
     }
 
-    public static void menuTemplate(ArrayList<PC> pcList, String[] menuOptions) {
+    public static Router createRouter() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the name for the Router: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Please enter the IP address for " + name + ": ");
+        String ipInput = scanner.nextLine();
+        IPAddress ip = createIP(ipInput);
+
+        return new Router(name);
+    }
+
+    public static void displayMainMenu(ArrayList<PC> pcList, ArrayList<Router> routerList) {
+        // Options displayed for the user to choose on what actions they want to take.
+        String[] menuOptions = {
+                "Create PC    ",
+                "Delete PC    ",
+                "Select PC    ",
+                "Create Router",
+                "Delete Router",
+                "Select Router",
+                "Exit Program "
+        };
+
         System.out.print("------------------------------------------------------------------------------------\n");
-        System.out.println("Options     \t\t\t| List of PC's");
+        System.out.println("Options     \t\t\t| List of Devices");
+        System.out.print("------------------------------------------------------------------------------------\n");
+
+        // Picks the largest array/list to iterate through and prints out all of the options from the menu as well as
+        // the devices to the right of the options.
+        int max1 = Math.max(pcList.size(), menuOptions.length);
+        int max2 = Math.max(max1, routerList.size());
+        // Iterate through the largest array and print out the menu.
+        for(int i = 0; i < max2; i++) {
+            int lineNum = i + 1;
+
+            if (i < menuOptions.length) {
+                // This is for printing out the menu and the devices.
+                System.out.print(lineNum + "." + " " + menuOptions[i] + "\t\t" + "|");
+            }
+            if (i < pcList.size()) {
+                // This is for printing out the pc's only, once all the options are printed.
+                PC pc = pcList.get(i);
+                System.out.print(" " + lineNum + "." + " " + pc.getName());
+            }
+            if (i < routerList.size()) {
+                // This is for printing out the router's only.
+                Router router = routerList.get(i);
+                System.out.print(" " + lineNum + "." + " " + router.getName());
+            }
+            // Print a new line after every line.
+            System.out.println();
+        }
+
+        // Some space for user input and the menu.
+        System.out.println();
+    }
+
+    public static void displayPCMenu(ArrayList<PC> pcList) {
+        // Options displayed for the user to choose on what actions they want to take.
+        String[] menuOptions = {
+                "Change PC name                     ",
+                "Change FastEthernet 0/0 IP address ",
+                "Connect to another device          ",
+                "Ping another device                ",
+                "Return to main menu                "
+        };
+
+        System.out.print("------------------------------------------------------------------------------------\n");
+        System.out.println("Options     \t\t\t\t\t\t\t\t| List of PC's");
         System.out.print("------------------------------------------------------------------------------------\n");
 
         // Picks the largest array/list to iterate through and prints out all of the options from the menu as well as
@@ -158,10 +284,9 @@ public class Main {
                 System.out.print(lineNum + "." + " " + menuOptions[i] + "\t\t" + "|");
             }
             if (i < pcList.size()) {
-                // This is for printing out the devices only, once all of the options are printed.
+                // This is for printing out the devices only, once all the options are printed.
                 PC pc = pcList.get(i);
-                //                                                                Needs to get the IPAddress object and then the IPAddress as a string
-                System.out.print(" " + lineNum + "." + " " + pc.getName() + " " + pc.getInterfaceFA00().getIpAddress().toString() + " " + pc.getInterfaceFA00().getMacAddress().toString());
+                System.out.print(" " + lineNum + "." + " " + pc.getName());
             }
             // Print a new line after every line.
             System.out.println();
@@ -171,28 +296,40 @@ public class Main {
         System.out.println();
     }
 
-    public static void printMainMenu(ArrayList<PC> pcList) {
+    public static void displayRouterMenu(ArrayList<Router> routerList) {
         // Options displayed for the user to choose on what actions they want to take.
-        String[] mainMenuOptions = {
-                "Create PC    ",
-                "Delete PC    ",
-                "Select PC    ",
-                "Exit Program "
+        String[] menuOptions = {
+                "Change Router name                    ",
+                "Change GigabitEthernet 0/0 IP address ",
+                "Change GigabitEthernet 0/1 IP address ",
+                "Change GigabitEthernet 0/2 IP address ",
+                "Connect to another device             ",
+                "Return to main menu                   "
         };
 
-        menuTemplate(pcList, mainMenuOptions);
+        System.out.print("------------------------------------------------------------------------------------\n");
+        System.out.println("Options    \t\t\t\t\t\t\t\t\t\t| List of Router's");
+        System.out.print("------------------------------------------------------------------------------------\n");
+
+        // Picks the largest array/list to iterate through and prints out all of the options from the menu as well as
+        // the devices to the right of the options.
+        for(int i = 0; i < Math.max(routerList.size(), menuOptions.length); i++) {
+            int lineNum = i + 1;
+
+            if (i < menuOptions.length) {
+                // This is for printing out the menu and the devices.
+                System.out.print(lineNum + "." + " " + menuOptions[i] + "\t\t" + "|");
+            }
+            if (i < routerList.size()) {
+                // This is for printing out the devices only, once all the options are printed.
+                Router router = routerList.get(i);
+                System.out.print(" " + lineNum + "." + " " + router.getName());
+            }
+            // Print a new line after every line.
+            System.out.println();
+        }
+
+        // Some space for user input and the menu.
+        System.out.println();
     }
-
-    public static void printPCMenu(ArrayList<PC> pcList) {
-        // Options displayed for the user to choose on what actions they want to take.
-        String[] deviceMenuOptions = {
-                "Change PC name        ",
-                "Change PC IP address  ",
-                "Connect to another PC ",
-                "Return to main menu   "
-        };
-
-        menuTemplate(pcList, deviceMenuOptions);
-    }
-
 }
--- src/PC.java
+++ src/PC.java
@@ -1,6 +1,7 @@
-public class PC extends Device{
+public class PC extends Device {
     // PC's only have one interface, and it is always a fastEthernet0/0.
     private Port fa00 = new Port("FastEthernet 0/0");
+    private IPAddress defaultGateway;
 
     PC(String name, IPAddress ipaddress) {
         super(name);
@@ -11,11 +12,14 @@ public class PC extends Device{
     PC(String name) {
         // Call the parent Device class. With the maximum amount of interfaces set to 1.
         super(name);
-    };
+    }
 
     public Port getPortFA00() { return fa00; }
 
     // You shouldn't be able to change the name of the interface, as they are always predefined. Only the IP can be
     // changed. MAC generated within the Port class randomly.
-    public void setFA00(IPAddress ipAddress) { this.fa00.setIpAddress(ipAddress); }
+    public void setPortFA00(IPAddress ipAddress) { this.fa00.setIpAddress(ipAddress); }
+
+    public IPAddress getDefaultGateway() { return defaultGateway; }
+    public void setDefaultGateway(IPAddress ipAddress) { this.defaultGateway = ipAddress; }
 }
