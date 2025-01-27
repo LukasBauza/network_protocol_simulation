3bcc5a9aaa44db3648c231c33a242864f8dba6f7
Lukas Bauza <lukasbauza.lb@proton.me>
2025-01-27 15:09:00 +0000

    Comments for Device.java
    Signed-off-by: Lukas Bauza <lukasbauza.lb@proton.me>

--- src/Device.java
+++ src/Device.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 abstract public class Device {
-    private String name;
-    private ARPTable arpTable = new ARPTable();
-    private ArrayList<NIC> nicList;
+    // Abstract class for holding data memebers and methods that are common within the Router and PC child classes.
 
+    private String name;                            // Name of the device
+    private ARPTable arpTable = new ARPTable();     // ARP table for the device.
+    private ArrayList<NIC> nicList;                 // Interfaces for the device
+
+    // Constructor which is used within the creation of the child class.
     public Device(String name) {
         this.name = name;
     }
+
+    // Get and set functions for retrieving data members, or changing their values.
 
     public String getName() { return name; }
 
