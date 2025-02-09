701e37d69471050e9b8b711e43065d97eb8b20aa
Lukas Bauza <lukasbauza.lb@proton.me>
2025-02-09 19:15:22 +0000

    Use the NIC list from the super class instead of creating local ones within Router.java
    Signed-off-by: Lukas Bauza <lukasbauza.lb@proton.me>

--- src/Router.java
+++ src/Router.java
@@ -1,27 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Router extends Device {
-    private NIC gig00 = new NIC("GigabitEthernet 0/0");
-    private NIC gig01 = new NIC("GigabitEthernet 0/1");
-    private NIC gig02 = new NIC("GigabitEthernet 0/2");
 
     public Router(String name) {
+        // Use the parent class constructor to add the name variable within the Device class.
         super(name);
+
+        // Set up the NIC for the Router, which is Gig 0/0, Gig 0/1, Gig 0/2. There is only a name for them set, as the
+        // user should be able to set the IP and subnet mask at a later time.
+        NIC gig00 = new NIC("GigabitEthernet 0/0");
+        NIC gig01 = new NIC("GigabitEthernet 0/1");
+        NIC gig02 = new NIC("GigabitEthernet 0/2");
+
+        // Add the NICs to the ArrayList of the nic list within the parent class.
+        super.setNICList(new ArrayList<>(List.of(gig00, gig01, gig02)));
     }
 
-    public NIC getPortGig00() { return gig00; }
+    public NIC getPortGig00() { return super.getNICList().get(0); }
+
     public void setPortGig00(IPAddress ipAddress, SubnetMask subnetMask) {
-        this.gig00.setIpAddress(ipAddress);
-        this.gig00.setSubnetMask(subnetMask);
+        super.getNICList().get(0).setIpAddress(ipAddress);
+        super.getNICList().get(0).setSubnetMask(subnetMask);
     }
 
-    public NIC getPortGig01() { return gig01; }
+    public NIC getPortGig01() { return super.getNICList().get(1); }
     public void setPortGig01(IPAddress ipAddress, SubnetMask subnetMask) {
-        this.gig01.setIpAddress(ipAddress);
-        this.gig01.setSubnetMask(subnetMask);
+        super.getNICList().get(1).setIpAddress(ipAddress);
+        super.getNICList().get(1).setSubnetMask(subnetMask);
     }
 
-    public NIC getPortGig02() { return gig02; }
+    public NIC getPortGig02() { return super.getNICList().get(2); }
     public void setPortGig02(IPAddress ipAddress, SubnetMask subnetMask) {
-        this.gig02.setIpAddress(ipAddress);
-        this.gig02.setSubnetMask(subnetMask);
+        super.getNICList().get(2).setIpAddress(ipAddress);
+        super.getNICList().get(2).setSubnetMask(subnetMask);
     }
 }
