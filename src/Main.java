ddd60d4e5ed3d3f4245b37cddc1e725fd542c9fe
Lukas Bauza <lukasbauza.lb@proton.me>
2025-03-10 19:32:29 +0000

    Window dynamically changes between custom network display, and prebuilt network display.
    Signed-off-by: Lukas Bauza <lukasbauza.lb@proton.me>

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,12 @@ import java.util.concurrent.atomic.AtomicReference;
 public class Main {
     public static void main(String[] args) {
 
-        // **** Start Menu ****
-        JFrame start_menu = new JFrame();
-
-        start_menu.setTitle("OSPF Simulation");
-        start_menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // Close the application, when pressing X
-        start_menu.setResizable(false);
-        start_menu.setSize(600, 300);
-        start_menu.setLayout(new GridLayout(0, 1));       // rows=0, cols=1. Makes it vertical.
+        JFrame frame = new MainMenuFrame();
+        frame.setTitle("OSPF Simulation");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // Close the application, when pressing X
+        frame.setResizable(false);
+        frame.setSize(600, 300);
+        frame.setLayout(new GridLayout(0, 1));       // rows=0, cols=1. Makes it vertical.
 
         JLabel welcomeLabel = new JLabel("""
                 <html>
@@ -30,19 +28,49 @@ public class Main {
         JButton customNetworkButton = new JButton("Custom Network");
         Font networkButtonFont = new Font(customNetworkButton.getFont().getName(), Font.BOLD, 20);
         customNetworkButton.setFont(networkButtonFont);
-        customNetworkButton.addActionListener(e -> {
-            System.out.println("Custom network button pressed.");
-        });
 
         JButton preconfiguredNetworkButton = new JButton("Preconfigured Network");
         preconfiguredNetworkButton.setFont(networkButtonFont);
+
+        frame.add(welcomeLabel);
+        frame.add(customNetworkButton);
+        frame.add(preconfiguredNetworkButton);
+        frame.setVisible(true);                                 // Make start_menu_frame visible
+
+        JLabel prebuiltNetworkLabel = new JLabel("Prebuilt Network");
+
+        JLabel customNetworkLabel = new JLabel("Custom Network");
+
         preconfiguredNetworkButton.addActionListener(e -> {
             System.out.println("Preconfigured network button pressed.");
+
+            frame.remove(welcomeLabel);
+            frame.remove(customNetworkButton);
+            frame.remove(preconfiguredNetworkButton);
+
+            frame.setTitle("OSPF Simulation: Prebuilt Network");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // Close the application, when pressing X
+            frame.setResizable(false);
+            frame.setSize(1000, 600);
+            frame.setLayout(new GridLayout());       // rows=0, cols=1. Makes it vertical.
+
+            frame.add(prebuiltNetworkLabel);
         });
 
-        start_menu.add(welcomeLabel);
-        start_menu.add(customNetworkButton);
-        start_menu.add(preconfiguredNetworkButton);
-        start_menu.setVisible(true);                                 // Make start_menu visible
+        customNetworkButton.addActionListener(e -> {
+            System.out.println("Custom network button pressed.");
+
+            frame.remove(welcomeLabel);
+            frame.remove(customNetworkButton);
+            frame.remove(preconfiguredNetworkButton);
+
+            frame.setTitle("OSPF Simulation: Custom Network");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // Close the application, when pressing X
+            frame.setResizable(false);
+            frame.setSize(1000, 600);
+            frame.setLayout(new GridLayout());       // rows=0, cols=1. Makes it vertical.
+
+            frame.add(customNetworkLabel);
+        });
     }
 }
