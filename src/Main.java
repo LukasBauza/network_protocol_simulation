cd02a5bea79a9489c251f72ffaa1ed06eb54ebf1
Lukas Bauza <lukasbauza.lb@proton.me>
2025-03-11 07:41:04 +0000

    Created PC and Router objects for buttons.
    Signed-off-by: Lukas Bauza <lukasbauza.lb@proton.me>

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,7 @@ import java.util.concurrent.atomic.AtomicReference;
 public class Main {
     public static void main(String[] args) {
 
-        JFrame frame = new MainMenuFrame();
-        frame.setTitle("OSPF Simulation");
+        JFrame frame = new JFrame("OSPF Simulation");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // Close the application, when pressing X
         frame.setResizable(false);
         frame.setSize(600, 300);
@@ -39,6 +38,9 @@ public class Main {
 
         JLabel prebuiltNetworkLabel = new JLabel("Prebuilt Network");
 
+        JButton[] routerButtons = getRouterJButtons();
+        JButton[] pcButtons = getPCButtons();
+
         JLabel customNetworkLabel = new JLabel("Custom Network");
 
         preconfiguredNetworkButton.addActionListener(e -> {
@@ -55,6 +57,14 @@ public class Main {
             frame.setLayout(new GridLayout());       // rows=0, cols=1. Makes it vertical.
 
             frame.add(prebuiltNetworkLabel);
+
+            for (JButton button : routerButtons) {
+                frame.add(button);
+            }
+
+            for (JButton button : pcButtons) {
+                frame.add(button);
+            }
         });
 
         customNetworkButton.addActionListener(e -> {
@@ -73,4 +83,36 @@ public class Main {
             frame.add(customNetworkLabel);
         });
     }
+
+    private static JButton[] getRouterJButtons() {
+        Router[] routers = {
+                new Router("R0"),
+                new Router("R1"),
+                new Router("R2"),
+                new Router("R3"),
+                new Router("R4")
+        };
+        return new JButton[]{
+                new JButton(routers[0].getName()),
+                new JButton(routers[1].getName()),
+                new JButton(routers[2].getName()),
+                new JButton(routers[3].getName()),
+                new JButton(routers[4].getName())
+        };
+    }
+
+    private static JButton[] getPCButtons() {
+        PC[] pcs = {
+                new PC("PC0", new IPAddress("192.168.1.1"), new SubnetMask("255.255.255.0")),
+                new PC("PC1", new IPAddress("192.168.2.1"), new SubnetMask("255.255.255.0")),
+                new PC("PC2", new IPAddress("192.168.3.1."), new SubnetMask("255.255.255.0")),
+                new PC("PC3", new IPAddress("192.168.4.1"), new SubnetMask("255.255.255.0")),
+        };
+        return new JButton[]{
+                new JButton("PC0"),
+                new JButton("PC1"),
+                new JButton("PC2"),
+                new JButton("PC3")
+        };
+    };
 }
